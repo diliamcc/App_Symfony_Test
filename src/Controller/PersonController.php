5a713aee702b0049158c11c168a419dfa42d9990
Diliam Cueto <cuetodiliam@gmail.com>
2023-04-28 13:07:48 -0500

    primer commit

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Person;
+use App\Form\PersonType;
+use App\Repository\PersonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+
+class PersonController extends AbstractController
+{
+    #[Route('/person', name: 'app_person_index', methods: ['GET'])]
+    public function index(PersonRepository $personRepository): Response
+    {
+        return $this->render('person/index.html.twig', [
+            'people' => $personRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_person_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PersonRepository $personRepository): Response
+    {
+        $person = new Person();
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $personRepository->add($person);
+            return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('person/new.html.twig', [
+            'person' => $person,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_person_show', methods: ['GET'])]
+    public function show(Person $person): Response
+    {
+        return $this->render('person/show.html.twig', [
+            'person' => $person,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_person_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Person $person, PersonRepository $personRepository): Response
+    {
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $personRepository->add($person);
+            return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('person/edit.html.twig', [
+            'person' => $person,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_person_delete', methods: ['POST'])]
+    public function delete(Request $request, Person $person, PersonRepository $personRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$person->getId(), $request->request->get('_token'))) {
+            $personRepository->remove($person);
+        }
+
+        return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Person.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+
+/**
+ * @ORM\Entity(repositoryClass=PersonRepository::class)
+ */
+class Person
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $companyName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Country::class, inversedBy="people")
+     */
+    private $country;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Priority::class, inversedBy="people")
+     */
+    private $priority;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): self
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getCountry(): ?country
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?country $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getPriority(): ?priority
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(?priority $priority): self
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+}
